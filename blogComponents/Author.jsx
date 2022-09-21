aba8cb9b2489c3304ba6edc42d20dbb7fc52cb1a
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-09-21 15:44:05 +0300

    updated blog page abd blog posts
    made changes ti the blog page  and blog post  to correct build errors

--- blogComponents/Author.jsx
+++ blogComponents/Author.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Author = ({ author }) => {
     return (
-        <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-10'>
+        <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-[#d05c14] '>
             <div className="absolute left-0 right-2 -top-14">
                 <Image
                     src={author.photo.url}
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,14 +3,14 @@ import React from 'react'
 import { getPosts, getPostsDetails } from '../../graphcsm'
 import { Author, PostDetail, Categories, Comments, PostWidget, CommentsForm } from '../../blogComponents'
 const PostDetails = ({ post }) => {
-    // console.log(post.slug)
+    console.log(post)
     return (
         <>
             <div className="container mx-auto px-10 mb-8">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                     <div className="col-span-1 lg:col-span-8">
                         <PostDetail post={post} />
-                        <Author author={post.author} />
+                        <Author author={post?.author} />
                         {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
                         {/* <CommentsForm slug={post.slug} />
                         <Comments slug={post.slug} /> */}
