10b180594fbabb222b79df0e00a697fe9f66bbf5
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-08-21 00:46:03 +0300

    Added blog categories and a loader for a better ui experience

--- graphcsm/index.js
+++ graphcsm/index.js
@@ -86,7 +86,42 @@ export const getPostsDetails = async (slug) => {
 
 }
 
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
 
+  const result = await request(graphqlApi, query, { slug });
+
+  return result.postsConnection.edges;
+};
 export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails(){
